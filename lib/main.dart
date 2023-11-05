a3aab3e2d1b442b82238ad227e96bf0bd6948dcd
Gushiken <gusiken0604@gmail.com>
2023-11-05 22:32:13 +0900

    57

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
-import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.orange,
-                  Colors.yellow,
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const StartScreen()),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
